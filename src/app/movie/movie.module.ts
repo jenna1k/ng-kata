b973563d3fd49e99cc7b15b030211e83942b7617
Dajung Kim <jenna_kim1@hotmail.com>
2019-08-08 15:41:50 +0900

    imported ReactiveForms & more material

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 // Component
 import { MovieHomeComponent } from './movie-home/movie-home.component';
 // Material
+import {MatSelectModule} from '@angular/material/select';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 
 
@@ -13,8 +16,11 @@ import { MatCardModule } from '@angular/material/card';
   imports: [
     CommonModule,
     HttpClientModule,
+    ReactiveFormsModule,
     // Material
-    MatCardModule
+    MatCardModule,
+    MatSelectModule,
+    MatFormFieldModule
   ],
   exports: [
     MovieHomeComponent
