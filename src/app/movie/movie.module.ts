f352ea1a126421b4eb1d41d1912938b6c4ced9e6
Dajung Kim <jenna_kim1@hotmail.com>
2019-08-11 17:03:02 +0200

    exported MovieHomeComponent from movie module

--- src/app/movie/movie.module.ts
+++ src/app/movie/movie.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-
+// Component
+import { MovieHomeComponent } from './movie-home/movie-home.component'
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     HttpClientModule
+  ],
+  exports : [
+    MovieHomeComponent
   ]
 })
 export class MovieModule { }
